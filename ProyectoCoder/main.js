f17d73aa023c8e32cd1a0b305f87cf54a8d65c52
Joseph <joseph98.gutierrez@gmail.com>
2022-01-20 23:23:39 -0500

    Tercera Entrega Proyecto Final

--- ProyectoCoder/main.js
+++ ProyectoCoder/main.js
@@ -15,27 +15,51 @@ let altoF = 40;
 
 // (12 x 17) - ( 10 x 16 )
 let tablero = [
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,1],
-[1,1,1,1,1,1,1,1,1,1,1,1]
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+let tableroCopia = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 // COLORES
@@ -50,220 +74,231 @@ let azul = '#0000cd';
 
 
 const fichaGrafico = [
-[
     [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
     ],
 
     [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
+        [
+            [0, 0, 0, 0],
+            [2, 2, 2, 2],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [2, 2, 2, 2],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0],
+            [0, 0, 2, 0]
+        ]
     ],
 
     [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
+        [
+            [0, 0, 0, 0],
+            [0, 0, 3, 3],
+            [0, 3, 3, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 3, 0],
+            [0, 0, 3, 3],
+            [0, 0, 0, 3],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [0, 0, 3, 3],
+            [0, 3, 3, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 3, 0],
+            [0, 0, 3, 3],
+            [0, 0, 0, 3],
+            [0, 0, 0, 0]
+        ]
     ],
 
     [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
+        [
+            [0, 0, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 4, 4],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 4],
+            [0, 0, 4, 4],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 4, 4],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 4],
+            [0, 0, 4, 4],
+            [0, 0, 4, 0],
+            [0, 0, 0, 0]
+        ]
+    ],
+
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 5, 5, 5],
+            [0, 5, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 5, 0],
+            [0, 0, 5, 0],
+            [0, 0, 5, 5],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 0, 5],
+            [0, 5, 5, 5],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 5, 5, 0],
+            [0, 0, 5, 0],
+            [0, 0, 5, 0],
+            [0, 0, 0, 0]
+        ]
+    ],
+
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 6, 6, 6],
+            [0, 0, 0, 6],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 6, 6],
+            [0, 0, 6, 0],
+            [0, 0, 6, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 6, 0, 0],
+            [0, 6, 6, 6],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 6, 0],
+            [0, 0, 6, 0],
+            [0, 6, 6, 0],
+            [0, 0, 0, 0]
+        ]
+    ],
+
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 7, 7, 7],
+            [0, 0, 7, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 7, 0],
+            [0, 0, 7, 7],
+            [0, 0, 7, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 7, 0],
+            [0, 7, 7, 7],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+
+        [
+            [0, 0, 7, 0],
+            [0, 7, 7, 0],
+            [0, 0, 7, 0],
+            [0, 0, 0, 0]
+        ]
     ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [2,2,2,2],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,2,0],
-        [0,0,2,0],
-        [0,0,2,0],
-        [0,0,2,0]
-    ],
-
-    [
-        [0,0,0,0],
-        [2,2,2,2],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,2,0],
-        [0,0,2,0],
-        [0,0,2,0],
-        [0,0,2,0]
-    ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [0,0,3,3],
-        [0,3,3,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,3,0],
-        [0,0,3,3],
-        [0,0,0,3],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,0,0],
-        [0,0,3,3],
-        [0,3,3,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,3,0],
-        [0,0,3,3],
-        [0,0,0,3],
-        [0,0,0,0]
-    ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [0,4,4,0],
-        [0,0,4,4],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,0,4],
-        [0,0,4,4],
-        [0,0,4,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,0,0],
-        [0,4,4,0],
-        [0,0,4,4],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,0,4],
-        [0,0,4,4],
-        [0,0,4,0],
-        [0,0,0,0]
-    ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [0,5,5,5],
-        [0,5,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,5,0],
-        [0,0,5,0],
-        [0,0,5,5],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,0,5],
-        [0,5,5,5],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,5,5,0],
-        [0,0,5,0],
-        [0,0,5,0],
-        [0,0,0,0]
-    ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [0,6,6,6],
-        [0,0,0,6],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,6,6],
-        [0,0,6,0],
-        [0,0,6,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,6,0,0],
-        [0,6,6,6],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,6,0],
-        [0,0,6,0],
-        [0,6,6,0],
-        [0,0,0,0]
-    ]
-],
-
-[
-    [
-        [0,0,0,0],
-        [0,7,7,7],
-        [0,0,7,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,7,0],
-        [0,0,7,7],
-        [0,0,7,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,7,0],
-        [0,7,7,7],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-
-    [
-        [0,0,7,0],
-        [0,7,7,0],
-        [0,0,7,0],
-        [0,0,0,0]
-    ]
-]
 ];
+
+
+function reseteaTablero(){
+    console.log('resetea');
+
+    for( py = 0; py < 21; py++){
+        for( px = 0; px < 12; px++){
+            tablero[py][px] = tableroCopia[py][px]
+        }
+    }
+}
 
 let pieza;
 
-let objPieza = function(){
+let objPieza = function () {
     this.x = 0;
     this.y = 0;
 
@@ -273,84 +308,166 @@ let objPieza = function(){
     this.retraso = 50;
     this.fotograma = 0;
 
-    this.nueva = function(){
+    this.nueva = function () {
         this.tipo = Math.floor(Math.random() * 7);
-        this.y = 5;
+        this.y = 0;
         this.x = 4;
     };
 
-    this.caer = function(){
-        if(this.fotograma < this.retraso){
+    this.compruebaSiPierde = function(){
+
+        let pierde = false;
+
+        for( px = 1; px < anchoTablero + 1; px++ ){
+            if( tablero[2][px] > 0 ){
+                pierde = true;
+            }
+        }
+
+        return pierde;
+
+    };
+
+
+
+
+
+
+    this.caer = function () {
+        if (this.fotograma < this.retraso) {
             this.fotograma++;
-        }else{
-            this.y++;
+        } else {
+
+            if(this.colision(this.angulo, this.y + 1, this.x)==false){
+                this.y++;
+            } else {
+                this.fijar();
+                this.nueva();
+
+                if(this.compruebaSiPierde() == true){
+                    reseteaTablero();
+                }
+
+            }
             this.fotograma = 0;
         }
     }
 
-    this.dibuja = function(){
-        for(py=0;py<4;py++){
-            for(px=0;px<4;px++){
-
-                if(fichaGrafico[this.tipo][this.angulo][py][px]!=0){
-
-
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 1){
-                ctx.fillStyle = rojo;
+    this.fijar = function(){
+        for( py = 0; py < 4; py++ ){
+            for( px = 0; px < 4; px++ ){
+                if( fichaGrafico[this.tipo][this.angulo][py][px] > 0 ){
+                    tablero[this.y + py][this.x + px] = fichaGrafico[this.tipo][this.angulo][py][px]
                 }
+            }
+        }
+    }
 
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 2){
-                    ctx.fillStyle = naranja;
-                }
+    this.colision = function(anguloNuevo, yNueva, xNueva ){
+        let resultado = false;
 
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 3){
-                    ctx.fillStyle = amarillo;
+        for( py = 0; py < 4; py++ ){
+            for( px = 0; px < 4; px++ ){
+                if( fichaGrafico[this.tipo][anguloNuevo][py][px] > 0 ){
+                    if( tablero[yNueva + py][xNueva + px] > 0 ){
+                        resultado = true
+                    }
                 }
-                
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 4){
+            }
+        }
+
+        return resultado;
+
+    };
+
+
+
+
+    this.dibuja = function () {
+        for (py = 0; py < 4; py++) {
+            for (px = 0; px < 4; px++) {
+
+                if (fichaGrafico[this.tipo][this.angulo][py][px] != 0) {
+
+
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 1) {
+                        ctx.fillStyle = rojo;
+                    }
+
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 2) {
+                        ctx.fillStyle = naranja;
+                    }
+
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 3) {
+                        ctx.fillStyle = amarillo;
+                    }
+
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 4) {
                         ctx.fillStyle = verde;
-                }
+                    }
 
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 5){
-                    ctx.fillStyle = cyan;
-                }
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 5) {
+                        ctx.fillStyle = cyan;
+                    }
 
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 6){
-                    ctx.fillStyle = azul;
-                }
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 6) {
+                        ctx.fillStyle = azul;
+                    }
 
-                if(fichaGrafico[this.tipo][this.angulo][py][px] == 7){
-                    ctx.fillStyle = morado;
-                }
+                    if (fichaGrafico[this.tipo][this.angulo][py][px] == 7) {
+                        ctx.fillStyle = morado;
+                    }
 
-                ctx.fillRect((this.x + px -1) * anchoF, (this.y + py - margenSuperior) * altoF, anchoF, altoF)
+                    ctx.fillRect((this.x + px - 1) * anchoF, (this.y + py - margenSuperior) * altoF, anchoF, altoF)
                 }
             }
         }
     };
 
-    this.rotar = function(){
-        if(this.angulo < 3){
-            this.angulo++;
-        }else{
-            this.angulo = 0;
+    this.rotar = function () {
+
+        let anguloNuevo = this.angulo;
+
+
+        if (anguloNuevo < 3) {
+            anguloNuevo++;
+        } else {
+            anguloNuevo = 0;
         }
+
+        if( this.colision(anguloNuevo, this.y, this.x) == false ){
+            this.angulo = anguloNuevo;
+        }
+
         console.log('Rotar')
     };
 
-    this.abajo = function() {
-        this.y++;
-        console.log('abajo')
+    this.abajo = function () {
+        
+        if( this.colision( this.angulo, this.y + 1, this.x ) == false){
+            this.y++;
+            console.log('abajo')
+        }
+
     };
 
-    this.derecha = function() {
-        this.x++;
-        console.log('derecha')
+    this.derecha = function () {
+
+        if( this.colision( this.angulo, this.y, this.x+1 ) == false){
+
+            this.x++;
+            console.log('derecha')
+        }
+
     };
 
-    this.izquierda = function() {
-        this.x--;
-        console.log('izquierda')
+    this.izquierda = function () {
+
+
+        if( this.colision( this.angulo, this.y, this.x-1 ) == false){
+            this.x--;
+            console.log('izquierda')
+        }
     };
 
     this.nueva();
@@ -359,42 +476,42 @@ let objPieza = function(){
 
 
 
-function dibujaTablero(){
-    for(py=margenSuperior;py<altoTablero;py++){
-        for(px=1;px<anchoTablero;px++){
+function dibujaTablero() {
+    for (py = margenSuperior; py < altoTablero; py++) {
+        for (px = 1; px < anchoTablero + 1; px++) {
 
-            if(tablero[py][px]!=0){
+            if (tablero[py][px] != 0) {
 
 
-            if(tablero[py][px] == 1){
-                ctx.fillStyle = rojo;
-            }
+                if (tablero[py][px] == 1) {
+                    ctx.fillStyle = rojo;
+                }
 
-            if(tablero[py][px] == 2){
-                ctx.fillStyle = naranja;
-            }
+                if (tablero[py][px] == 2) {
+                    ctx.fillStyle = naranja;
+                }
 
-            if(tablero[py][px] == 3){
-                ctx.fillStyle = amarillo;
-            }
-            
-            if(tablero[py][px] == 4){
-                ctx.fillStyle = verde;
-            }
+                if (tablero[py][px] == 3) {
+                    ctx.fillStyle = amarillo;
+                }
 
-            if(tablero[py][px] == 5){
-                ctx.fillStyle = cyan;
-            }
+                if (tablero[py][px] == 4) {
+                    ctx.fillStyle = verde;
+                }
 
-            if(tablero[py][px] == 6){
-                ctx.fillStyle = azul;
-            }
+                if (tablero[py][px] == 5) {
+                    ctx.fillStyle = cyan;
+                }
 
-            if(tablero[py][px] == 7){
-                ctx.fillStyle = morado;
-            }
+                if (tablero[py][px] == 6) {
+                    ctx.fillStyle = azul;
+                }
 
-            ctx.fillRect((px-1)*anchoF, (py-margenSuperior) * altoF, anchoF, altoF)
+                if (tablero[py][px] == 7) {
+                    ctx.fillStyle = morado;
+                }
+
+                ctx.fillRect((px - 1) * anchoF, (py - margenSuperior) * altoF, anchoF, altoF)
             }
         }
     }
@@ -403,23 +520,23 @@ function dibujaTablero(){
 
 
 function inicializaTeclado() {
-    document.addEventListener('keydown', function(tecla){
-        if(tecla.keyCode == 38){
+    document.addEventListener('keydown', function (tecla) {
+        if (tecla.keyCode == 38) {
             pieza.rotar();
         }
-        if(tecla.keyCode == 40){
+        if (tecla.keyCode == 40) {
             pieza.abajo();
         }
-        if(tecla.keyCode == 37){
+        if (tecla.keyCode == 37) {
             pieza.izquierda();
         }
-        if(tecla.keyCode == 39){
+        if (tecla.keyCode == 39) {
             pieza.derecha();
         }
     })
 }
 
-function inicializa(){
+function inicializa() {
 
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -431,12 +548,12 @@ function inicializa(){
 
     inicializaTeclado();
 
-    setInterval(function() {
+    setInterval(function () {
         principal();
-    },1000/FPS);
+    }, 1000 / FPS);
 }
 
-function borraCanvas(){
+function borraCanvas() {
     canvas.width = anchoCanvas;
     canvas.height = altoCanvas;
 }
